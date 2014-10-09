e2b2486f80abed4ad43aa6770382873dba9024c4
situ0020 <situ0020@algonquinlive.com>
2014-10-09 09:56:51 -0400

    Added shuttle animation and fade

--- scrolling-effects-starter-code/js/main.js
+++ scrolling-effects-starter-code/js/main.js
@@ -3,6 +3,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $shuttleSection = $('.shuttle-section');
+var $shuttle = $('.shuttle');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -13,4 +15,8 @@ $win.on('scroll', function () {
 
 $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
 }, { offset: '50%' });
